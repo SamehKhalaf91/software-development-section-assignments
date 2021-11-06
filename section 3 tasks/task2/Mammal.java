b132472c5e1c0c2eaec84a6a6f4a6bcb83adbb4b
SamehKhalaf91 <93044398+SamehKhalaf91@users.noreply.github.com>
2021-11-06 23:15:32 +0200

    Add files via upload

---
+++ section 3 tasks/task2/Mammal.java
@@ -0,0 +1,17 @@
+
+public class Mammal {
+	public void eat() {
+		System.out.println("food of mammal");
+	}
+	public void travel() {
+		System.out.println("travels of mammal");
+	}
+	public int NoOfLegs(int num) {
+		return num;
+	}
+	public String FavFood(String favFood) {
+		return favFood;
+	}
+
+
+}
